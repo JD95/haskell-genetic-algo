e480c27fea3eeef4596ff05a28e9b1acedb7a545
Jeff <jeffreydwyer95@hotmail.com>
2017-10-07 09:10:23 -0700

    Algo now runs

--- src/Main.hs
+++ src/Main.hs
@@ -1,36 +1,25 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Prelude ()
-import Protolude
+import           Prelude (String, foldr1)
+import           Protolude
 
-import Data.Semigroup ((<>))
-import Options.Applicative
-import Lens.Micro.Platform
-import System.Random
-import Data.Functor.Foldable
-import Control.Monad.Free
+import           Control.Arrow
+import           Control.Monad.Free
+import           Control.Parallel
+import           Control.Parallel.Strategies
+import           Data.Functor.Foldable
+import           Data.Semigroup ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.Read as T
+import           Data.Vector (Vector(..))
 import qualified Data.Vector as V
-import Data.Vector (Vector(..))
-
-data Options = Options { message :: Text }
-
-opts :: Parser Options
-opts = Options <$> option auto
-     ( long "message"
-    <> short 'm'
-    <> metavar "MESSAGE"
-    <> help "The message to print"
-    <> showDefault
-    <> value "Hello, World!"
-      )
-
-optsInfo :: ParserInfo Options
-optsInfo = info (opts <**> helper)
-  ( fullDesc
-  <> progDesc "Print a simple message"
-  <> header "genetic-algo - a minimal application"
-  )
+import qualified Data.Vector.Algorithms.Merge as V
+import           Lens.Micro.Platform
+import           Options.Applicative
+import           System.Random
+import           Text.Read (readEither)
 
 data Gene
   = Gene
@@ -43,38 +32,105 @@ data Gene
 
 data TData = TData Double Double Double deriving (Show, Eq)
 
-initGene :: RandomGen g => State g Gene
-initGene = undefined
+initPop :: RandomGen g => Int -> State g (Vector Gene) 
+initPop = fmap V.fromList . sequence . flip replicate randomGene 
+
+withinRange :: Gene -> TData -> Bool
+withinRange g (TData d1 d2 _)
+  = and [ a g <= d1
+        , d1 <= b g
+        , c g <= d2
+        , d2 <= d g
+        ] 
 
 calcFit :: Vector TData -> Gene -> (Gene, Double)
 calcFit tData gene = (gene, sum . fmap (f gene) $ tData) 
  where f :: Gene -> TData -> Double
-       f t g = undefined
+       f g t@(TData d1 d2 profit)
+         | withinRange g t = if e g then -1 * profit else profit
+         | otherwise = 0.0
+
+lower :: Applicative f => f (a -> b) -> (a -> f b)
+lower f a = f <*> pure a 
+
+lower2 :: Applicative f => f (a -> b -> c) -> (a -> b -> f c)
+lower2 = fmap lower . lower
 
 reproduce :: RandomGen g => Gene -> Gene -> State g Gene
-reproduce = undefined
+reproduce = lower2 $ randomlyApply identity flip f 
+  where f p1 p2 = Gene (a p1) (b p1) (c p2) (d p2) (e p2)
+
+randomlyApply :: RandomGen g => (a -> b) -> (a -> b) -> a -> State g b
+randomlyApply f g a = do
+  chance <- state (random @Float) 
+  if chance > 0.5
+    then pure (f a) 
+    else pure (g a) 
+
+randomGene :: RandomGen g => State g Gene
+randomGene = do
+  a <- state (random @Double)
+  b <- state (random @Double)
+  c <- state (random @Double)
+  d <- state (random @Double)
+  e <- state (random @Bool)
+  pure (Gene a b c d e)
 
 mutate :: RandomGen g => Gene -> State g Gene
-mutate = undefined
+mutate = join . randomlyApply pure (const randomGene)
 
 pairOff :: [a] -> [(a,a)]
 pairOff [] = []
 pairOff [x] = []
 pairOff (x:y:rest) = (x,y) : pairOff rest
 
+elitist :: Vector (Gene, Double) -> Vector Gene
+elitist = fmap fst . V.modify (V.sortBy (comparing snd))   
+
+tornament :: Vector (Gene, Double) -> Vector Gene
+tornament = undefined
+
+inParallel :: Traversable t => t a -> t a
+inParallel = withStrategy (parTraversable rpar)
+
 train :: RandomGen g
-      => (Vector (Gene, Double)
-      -> Vector Gene)
+      => (Vector (Gene, Double) -> Vector Gene)
       -> Vector TData
       -> Vector Gene
       -> State g (Vector Gene)
-train select tData genes = do
-  let survivors = select . fmap (calcFit tData) $ genes
-  babies <- sequence . V.fromList . fmap (uncurry reproduce) . pairOff . V.toList $ survivors 
-  pure (V.concat [survivors, babies])
+train select tData
+  = uncurry (liftA2 (<>)) -- ^ Combine survivors and babies
+  . (pure &&& repo)       -- ^ Reproduce
+  . select                -- ^ Use Selection
+  . inParallel 
+  . fmap (calcFit tData)  -- ^ Calculate fitness
+
+  where repo = sequence . V.fromList
+             . fmap (mutate <=< uncurry reproduce)
+             . pairOff . V.toList 
+
+parseData :: Text -> Either String [TData]
+parseData = sequence . fmap parseLine . T.lines
+  where parseLine = parse <=< sequence
+                  . fmap (readEither @Double)
+                  . fmap toS . T.splitOn "\t"
+        parse [a,b,c] = Right (TData a b c)
+        parse _ = Left "Invalid Data"
+
+runAlgo :: Vector TData -> IO ()
+runAlgo tdata = do
+  g <- getStdGen
+  popSize <- pure 2
+  gens <- pure 2
+  let result = flip evalState g $ do
+        genes <- initPop popSize 
+        foldr1 (<=<) (replicate gens (train elitist tdata)) genes
+  print "Results!"
+  mapM_ print result
 
 main :: IO ()
 main = do
-  options <- execParser optsInfo
-  putStrLn (message options)
-
+  contents <- parseData <$> readFile "genAlgData1.txt"
+  case contents of
+    Left e -> putStrLn $ "ERROR: " <> e
+    Right tdata -> runAlgo $ V.fromList tdata
